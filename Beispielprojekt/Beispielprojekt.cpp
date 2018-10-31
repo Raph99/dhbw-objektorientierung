561befcfdc2142b04c6644c76a00a094e493517f
koerber.raphael <koerber.raphael@347A16>
2018-10-31 13:09:48 +0100

    tutorial beendet

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -18,100 +18,52 @@ class GameWindow : public Gosu::Window
 public:
 	Gosu::Image bild;
 	GameWindow()
-		: Window(800, 600)
+		: Window(640, 480)
 		, bild("rakete.png")
-		// Rakete startet in der Mitte des Bildschirmes
-		, pos(graphics().width() / 2.0, graphics().height() / 2.0)
 	{
 		set_caption("Gosu Tutorial Game mit Git");
-
-		// Erzeuge Planeten
-		planets.push_back(Planet({ 200.0, 200.0 }, 0.1, "planet1.png"));
-		planets.push_back(Planet({ 600.0, 200.0 }, 0.1, "planet2.png"));
-		planets.push_back(Planet({ 400.0, 500.0 }, 0.1, "planet3.png"));
 	}
 
 	// wird bis zu 60x pro Sekunde aufgerufen.
 	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
-	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
-	void draw() override
-	{
-		bild.draw_rot(pos.get_x(), pos.get_y(), 10.0,
-			rot, // Rotationswinkel in Grad
-			0.5, 0.5 // Position der "Mitte"
-		);
-
-		auto g2 = (gravity * 1000000000000.0).log();
-
-		Vektor2d rose(50.0, 50.0);
-		auto g = rose - g2;
-		auto s = rose + speed * 1000.0;
-
-		graphics().draw_line(pos.get_x(), pos.get_y(), Gosu::Color::GREEN, input().mouse_x(), input().mouse_y(), Gosu::Color::GREEN, -10.0);
-		graphics().draw_line(rose.get_x(), rose.get_y(), Gosu::Color::RED, g.get_x(), g.get_y(), Gosu::Color::RED, 10.0);
-		graphics().draw_line(rose.get_x(), rose.get_y(), Gosu::Color::BLUE, s.get_x(), s.get_y(), Gosu::Color::BLUE, 10.0);
-
-		for (auto planet : planets) {
-			planet.draw();
-		}
-	}
-
-	double rot = 0.0;
-	Vektor2d pos, speed, gravity;
-	double accel = 0.0;
-	std::vector<Planet> planets;
-
-	// Wird 60x pro Sekunde aufgerufen
+	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinktwaw
+	int x = 0;
+	int y = 0;
+	double a = 0;
+	double b = 0;
+	double angle = 0;
+	
 	void update() override
 	{
-		// Geschwindigkeit führt zu Positionsänderung
-		pos += speed * DT;
+		x = input().mouse_x();
+		y = input().mouse_y();
 
-		// Beschleunigung während "W" gedrückt
-		if (input().down(Gosu::KB_W)) {
-			accel += 0.00001;
-		}
-		else {
-			accel -= 0.00002;
-		}
-
-		// Maximale Beschleunigung
-		accel = Gosu::clamp(accel, 0.0, 0.0001);
-
-		// Geschwindigkeit wird in Flugrichtung geändert
-		speed += Vektor2d::from_angle(rot, accel);
-
-
-		gravity = Vektor2d();
-
-		// Planeten ziehen Raumschiff an
-		for (auto planet : planets) {
-
-			// Entfernung im Quadrat
-			double dist = (pos - planet.pos).length_squared();
-
-			// Winkel des Vektors vom Raumschiff zum Planet
-			double angle = pos.angle(planet.pos);
-
-			// Anziehungskraft
-			double pull = planet.mass / dist;
-
-			// Kollision
-			if (dist < 1000) {
-				speed = Vektor2d::from_angle(angle, pull);
-				break;
+		if (input().down(Gosu::ButtonName::MS_LEFT))
+		{
+			for (int i = 0; i <= 360; i++)
+			{
+				a--;
 			}
-			gravity += Vektor2d::from_angle(angle, pull);
 		}
+		if (input().down(Gosu::ButtonName::MS_RIGHT))
+		{
+			for (int i = 0; i <= 360; i++)
+			{
+				a++;
+			}
+		}
+	}
 
-		speed += gravity;
-
-		// Raumschiff in Richtung Mauszeiger drehen
-		double angle = pos.angle({ input().mouse_x(), input().mouse_y() });
-		rot -= Gosu::angle_diff(angle, rot) / 36.0;
+	void draw() override
+	{
+		Gosu::Graphics::draw_line(
+			x, y, Gosu::Color::RED,
+			200, 10, Gosu::Color::RED,
+			0.0
+		);
+		bild.draw_rot(x, y,0.0, a ,0.5, 0.5);
 	}
 };
-
 // C++ Hauptprogramm
 int main()
 {
